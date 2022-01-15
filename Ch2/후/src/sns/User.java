a2cf2e2c92ebce0a4d835a453b1e6f001f27d8fd
who-hoo <who.ho3ov@gmail.com>
2022-01-15 17:34:20 +0900

    :sparkles: 각 사용자의 이름을 받아들이는 생성자 작성

--- Ch2/후/src/sns/User.java
+++ Ch2/후/src/sns/User.java
@@ -1,9 +1,15 @@
 package sns;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class User {
 
     private String name;
     private Set<User> friends;
+
+    public User(String name) {
+        this.name = name;
+        this.friends = new HashSet<>();
+    }
 }
