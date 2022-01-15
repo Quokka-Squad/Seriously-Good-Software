2268d72de0465d2dc091c0eb745cc175722307e3
who-hoo <who.ho3ov@gmail.com>
2022-01-16 00:22:19 +0900

    :ok_hand: 쿠킴의 리뷰 반영
    문자열 비교 대신 메서드 리턴 값 자체에 대한 비교를 통한 테스트 코드 작성

--- Ch1/후/src/test/java/myFirstImplementation/ContainerTest.java
+++ Ch1/후/src/test/java/myFirstImplementation/ContainerTest.java
@@ -16,15 +16,21 @@ class ContainerTest {
         a.addWater(12);
         d.addWater(8);
         a.connectTo(b);
-        assertEquals("6.0 6.0 0.0 8.0",
-            a.getAmount() + " " + b.getAmount() + " " + c.getAmount() + " " + d.getAmount());
+        assertEquals(6.0, a.getAmount());
+        assertEquals(6.0, b.getAmount());
+        assertEquals(0.0, c.getAmount());
+        assertEquals(8.0, d.getAmount());
 
         b.connectTo(c);
-        assertEquals("4.0 4.0 4.0 8.0",
-            a.getAmount() + " " + b.getAmount() + " " + c.getAmount() + " " + d.getAmount());
+        assertEquals(4.0, a.getAmount());
+        assertEquals(4.0, b.getAmount());
+        assertEquals(4.0, c.getAmount());
+        assertEquals(8.0, d.getAmount());
 
         b.connectTo(d);
-        assertEquals("5.0 5.0 5.0 5.0",
-            a.getAmount() + " " + b.getAmount() + " " + c.getAmount() + " " + d.getAmount());
+        assertEquals(5.0, a.getAmount());
+        assertEquals(5.0, b.getAmount());
+        assertEquals(5.0, c.getAmount());
+        assertEquals(5.0, d.getAmount());
     }
 }
