dca0e93fc6355469043f9b21266512bd7458fd79
who-hoo <who.ho3ov@gmail.com>
2022-01-22 01:11:56 +0900

    :sparkles: 물을 수조에 넣거나 빼는 addWater 구현

--- code/후/src/main/java/ch3/SpeedContainer3.java
+++ code/후/src/main/java/ch3/SpeedContainer3.java
@@ -17,4 +17,12 @@ public class SpeedContainer3 {
         SpeedContainer3 root = findRootAndCompress();
         return root.amount;
     }
+
+    public void addWater(double amount) {
+        SpeedContainer3 root = findRootAndCompress();
+        if (root.amount + amount < 0) {
+            throw new IllegalArgumentException("수조에 물이 충분히 들어있지 않습니다.");
+        }
+        root.amount += amount / root.size;
+    }
 }
