18b1c3afba57d72cf68cc477e5e0e20837d2adbe
who-hoo <who.ho3ov@gmail.com>
2022-01-22 01:06:10 +0900

    :sparkles: 그룹의 대표를 찾으며 압축을 진행하는 findRootAndCompress 구현

--- code/후/src/main/java/ch3/SpeedContainer3.java
+++ code/후/src/main/java/ch3/SpeedContainer3.java
@@ -5,4 +5,11 @@ public class SpeedContainer3 {
     private double amount;
     private SpeedContainer3 parent = this;
     private int size = 1;
+
+    private SpeedContainer3 findRootAndCompress() {
+        if (parent != this) {
+            parent = parent.findRootAndCompress();
+        }
+        return parent;
+    }
 }
