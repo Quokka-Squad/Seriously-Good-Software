d798aaad2b2f0c4316c07aa67983e170932630f3
함석호 <kalejuice02@gmail.com>
2022-01-23 18:30:37 +0900

    Appliance가 이미 켜져있거나 꺼져있을 경우를 고려하여 코드 수정

--- code/포키/src/main/java/chap03/Main.java
+++ code/포키/src/main/java/chap03/Main.java
@@ -5,10 +5,15 @@ public class Main {
     public static void main(String[] args) throws Exception{
         Appliance tv = new Appliance(150);
         Appliance radio = new Appliance(30);
+        Appliance dd = new Appliance(3001);
         Grid grid = new Grid(3000);
 
         tv.plugIn(grid);
         radio.plugIn(grid);
+        dd.plugIn(grid);
+
+
+
 
         System.out.println(grid.residualPower());
         tv.on();
@@ -16,6 +21,9 @@ public class Main {
         radio.on();
         System.out.println(grid.residualPower());
         radio.off();
+        System.out.println(grid.residualPower());
+
+        dd.on();
         System.out.println(grid.residualPower());
     }
 
