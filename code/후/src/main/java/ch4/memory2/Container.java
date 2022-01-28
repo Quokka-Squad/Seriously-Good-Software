6638a82f13580898f61d91c185a3fbf282bcbf06
who-hoo <who.ho3ov@gmail.com>
2022-01-28 23:46:28 +0900

    :sparkles: 일반 배열을 이용해 최적화할 Container 클래스 생성(Memory1 version)

---
+++ code/후/src/main/java/ch4/memory2/Container.java
@@ -0,0 +1,52 @@
+package ch4.memory2;
+
+import java.util.*;
+
+public class Container {
+
+    private List<Container> group;
+    private float amount;
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void connectTo(Container other) {
+        if (group == null) {
+            group = new ArrayList<>();
+            group.add(this);
+        }
+        if (other.group == null) {
+            other.group = new ArrayList<>();
+            other.group.add(other);
+        }
+        if (group == other.group) {
+            return;
+        }
+
+        int size1 = group.size();
+        int size2 = other.group.size();
+        float total1 = amount * size1;
+        float total2 = other.amount * size2;
+        float newAmount = (total1 + total2) / (size1 + size2);
+
+        group.addAll(other.group);
+        for (Container c : other.group) {
+            c.group = group;
+        }
+        for (Container c : group) {
+            c.amount = newAmount;
+        }
+    }
+
+    public void addWater(float amount) {
+        if (group == null) {
+            this.amount += amount;
+            return;
+        }
+        float amountPerContainer = amount / group.size();
+        for (Container c : group) {
+            c.amount += amountPerContainer;
+        }
+    }
+}
