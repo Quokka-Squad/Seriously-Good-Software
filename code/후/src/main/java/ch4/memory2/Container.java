521fddcd29bdc6d15b929a9d0bed4f7757bd0cf8
who-hoo <who.ho3ov@gmail.com>
2022-01-29 01:02:05 +0900

    :zap: 일반 배열을 사용하여 최적화

--- code/후/src/main/java/ch4/memory2/Container.java
+++ code/후/src/main/java/ch4/memory2/Container.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Container {
 
-    private List<Container> group;
+    private Container[] group;
     private float amount;
 
     public float getAmount() {
@@ -13,29 +13,33 @@ public class Container {
 
     public void connectTo(Container other) {
         if (group == null) {
-            group = new ArrayList<>();
-            group.add(this);
+            group = new Container[]{this};
         }
         if (other.group == null) {
-            other.group = new ArrayList<>();
-            other.group.add(other);
+            other.group = new Container[]{other};
         }
         if (group == other.group) {
             return;
         }
 
-        int size1 = group.size();
-        int size2 = other.group.size();
+        int size1 = group.length;
+        int size2 = other.group.length;
         float total1 = amount * size1;
         float total2 = other.amount * size2;
         float newAmount = (total1 + total2) / (size1 + size2);
 
-        group.addAll(other.group);
-        for (Container c : other.group) {
-            c.group = group;
-        }
+        Container[] newGroup = new Container[size1 + size2];
+
+        int i = 0;
         for (Container c : group) {
+            c.group = newGroup;
             c.amount = newAmount;
+            newGroup[i++] = c;
+        }
+        for (Container c : other.group) {
+            c.group = newGroup;
+            c.amount = newAmount;
+            newGroup[i++] = c;
         }
     }
 
@@ -44,7 +48,7 @@ public class Container {
             this.amount += amount;
             return;
         }
-        float amountPerContainer = amount / group.size();
+        float amountPerContainer = amount / group.length;
         for (Container c : group) {
             c.amount += amountPerContainer;
         }
