26b9172c616053fc7a5f47481b3cdd4601eab930
who-hoo <who.ho3ov@gmail.com>
2022-02-04 19:14:20 +0900

    :sparkles: 메서드 호출 시마다 사전 조건과 사후 조건을 검사할 Container 클래스 생성(Reference version)

---
+++ code/후/src/main/java/ch5/contracts/Container.java
@@ -0,0 +1,46 @@
+package ch5.contracts;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Container {
+
+    private Set<Container> group;
+    private double amount;
+
+    public Container() {
+        group = new HashSet<>();
+        group.add(this);
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void connectTo(Container other) {
+        if (group == other.group) {
+            return;
+        }
+
+        int size1 = group.size(),
+            size2 = other.group.size();
+        double total1 = amount * size1,
+            total2 = amount * size2,
+            newAmount = (total1 + total2) / (size1 + size2);
+
+        group.addAll(other.group);
+        for (Container c : other.group) {
+            c.group = group;
+        }
+        for (Container c : group) {
+            c.amount = newAmount;
+        }
+    }
+
+    public void addWater(double amount) {
+        double amountPerContainer = amount / group.size();
+        for (Container c : group) {
+            c.amount += amountPerContainer;
+        }
+    }
+}
