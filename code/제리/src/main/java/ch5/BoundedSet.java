c9e7f4a9b736af45943c3ad9393c5bc247ee62aa
hsjang <hsjang0405@gmail.com>
2022-02-04 16:03:25 +0900

    ✨  사전조건 구현

--- code/제리/src/main/java/ch5/BoundedSet.java
+++ code/제리/src/main/java/ch5/BoundedSet.java
@@ -2,18 +2,25 @@ package ch5;
 
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Objects;
 
 public class BoundedSet<T> {
 
-	private int maxSize;
-	private Deque<T> deque;
+	private final int maxSize;
+	private final Deque<T> deque;
 
 	public BoundedSet(int maxSize) {
+		if (maxSize <= 0) {
+			throw new IllegalArgumentException();
+		}
 		this.maxSize = maxSize;
 		this.deque = new LinkedList<>();
 	}
 
 	public void add(T elem) {
+		if (Objects.isNull(elem)) {
+			throw new NullPointerException();
+		}
 		if (deque.size() == maxSize) {
 			if (deque.contains(elem)) {
 				deque.remove(elem);
@@ -28,6 +35,9 @@ public class BoundedSet<T> {
 	}
 
 	public boolean contains(T elem) {
+		if (Objects.isNull(elem)) {
+			throw new NullPointerException();
+		}
 		return deque.contains(elem);
 	}
 
