0a13a577b5200f4b7cba836b7f6310d4e0af0b4a
who-hoo <who.ho3ov@gmail.com>
2022-02-05 01:09:17 +0900

    :sparkles: BoundedSet 기능 구현

--- code/후/src/main/java/ch5/boundedset/BoundedSet.java
+++ code/후/src/main/java/ch5/boundedset/BoundedSet.java
@@ -1,28 +1,44 @@
 package ch5.boundedset;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BoundedSet<T> {
 
+    private final Queue<T> data;
     private final int MAX_SIZE;
 
     public BoundedSet(int maxSize) {
+        this.data = new LinkedList<>();
         this.MAX_SIZE = maxSize;
     }
 
     public void add(T elem) {
-        // TODO
         // 1. 사전 조건 : 인자는 null이 아님
         // 2. 패널티 : NPE
+        if (elem == null) {
+            throw new NullPointerException();
+        }
+
         // 3. 사후 조건 : 주어진 요소를 크기가 정해진 집합에 추가
         // 요소의 개수가 용량을 초과하면 가장 오래된 요소 제거
+        if (data.size() == MAX_SIZE) {
+            data.poll();
+        }
         // 이미 존재하는 요소를 다시 추가하면 해당 요소를 갱신
+        data.remove(elem);
+        data.offer(elem);
     }
 
     public boolean contains(T elem) {
-        // TODO
         // 1. 사전 조건 : 인자는 null이 아님
         // 2. 패널티 : NPE
+        if (elem == null) {
+            throw new NullPointerException();
+        }
+
         // 3. 사후 조건 : 인자로 주어진 요소가 집합에 이미 존재하면 true 리턴
         // 이 메서드는 집합을 변경하지 않는다.
-        return false;
+        return data.contains(elem);
     }
 }
