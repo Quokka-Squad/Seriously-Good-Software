23ff75b0f0507b400425eef998731a9ea0fd7d0c
who-hoo <who.ho3ov@gmail.com>
2022-02-04 22:58:37 +0900

    :recycle: addWater의 계약 검사 추가

--- code/후/src/main/java/ch5/contracts/Container.java
+++ code/후/src/main/java/ch5/contracts/Container.java
@@ -39,8 +39,47 @@ public class Container {
 
     public void addWater(double amount) {
         double amountPerContainer = amount / group.size();
+        // 1. 사전 조건 검사
+        if (this.amount + amountPerContainer < 0) {
+            throw new IllegalArgumentException(
+                "Not enough water to match the addWater request."
+            );
+        }
+        // 2. 사후 조건 검사를 위한 데이터 저장
+        double oldTotal = 0;
+        assert (oldTotal = groupAmount()) >= 0;
+        // 3. 실제 작업 수행
         for (Container c : group) {
             c.amount += amountPerContainer;
         }
+        // 4. 사후 조건 검사
+        assert postAddWater(oldTotal, amount) :
+            "addWater failed its post condition!";
+    }
+
+    private double groupAmount() {
+        double total = 0;
+        for (Container c : group) {
+            total += c.amount;
+        }
+        return total;
+    }
+
+    private boolean postAddWater(double oldTotal, double addedAmount) {
+        return isGroupBalanced() && almostEqual(groupAmount(), oldTotal + addedAmount);
+    }
+
+    private boolean isGroupBalanced() {
+        for (Container c : group) {
+            if (c.amount != amount) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean almostEqual(double x, double y) {
+        final double EPSILON = 1E-4;
+        return Math.abs(x - y) < EPSILON;
     }
 }
