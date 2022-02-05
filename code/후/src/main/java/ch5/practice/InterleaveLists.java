00b5fff44ff88b457f5a382ccf221f9ab62934a4
who-hoo <who.ho3ov@gmail.com>
2022-02-05 11:47:54 +0900

    :recycle: 제네릭 추가

--- code/후/src/main/java/ch5/practice/InterleaveLists.java
+++ code/후/src/main/java/ch5/practice/InterleaveLists.java
@@ -6,14 +6,14 @@ import java.util.List;
 
 public class InterleaveLists {
 
-    public static List interleaveLists(List a, List b) {
+    public static <T> List<T> interleaveLists(List<? extends T> a, List<? extends T> b) {
         if (a == null || b == null) {
             throw new NullPointerException();
         }
         if (a.size() != b.size()) {
             throw new IllegalArgumentException();
         }
-        List interleave = new ArrayList();
+        List<T> interleave = new ArrayList<>();
         for (int i = 0; i < a.size(); i++) {
             interleave.add(a.get(i));
             interleave.add(b.get(i));
@@ -22,13 +22,13 @@ public class InterleaveLists {
         return interleave;
     }
 
-    private static boolean interleaveCheckPost(List a, List b, List result) {
+    private static boolean interleaveCheckPost(List<?> a, List<?> b, List<?> result) {
         if (result.size() != a.size() + b.size()) {
             return false;
         }
         boolean odd = true;
-        Iterator ia = a.iterator();
-        Iterator ib = b.iterator();
+        Iterator<?> ia = a.iterator();
+        Iterator<?> ib = b.iterator();
         for (Object elem : result) {
             if (odd && elem != ia.next()) {
                 return false;
