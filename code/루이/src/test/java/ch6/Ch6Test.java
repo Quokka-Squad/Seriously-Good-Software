7c6e5cb68bca3444ffa4800fc78a00c92cecbef1
Louie <dhdustnr0134@naver.com>
2022-02-10 08:47:27 +0900

    프로젝트 gradle로 변경

---
+++ code/루이/src/test/java/ch6/Ch6Test.java
@@ -0,0 +1,14 @@
+package ch6;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Ch6Test {
+
+    @Test
+    void name() {
+//        assertThrows()
+    }
+}
