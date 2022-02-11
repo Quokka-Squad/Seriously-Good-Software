2f3e698101497288cf3806fe780d4e88cb1ca2a6
Louie <dhdustnr0134@naver.com>
2022-02-11 16:11:40 +0900

    testNewContainerIsEmpty 테스트 메서드 구현

--- code/루이/src/test/java/ch6/UnitTests.java
+++ code/루이/src/test/java/ch6/UnitTests.java
@@ -1,15 +1,28 @@
 package ch6;
 
-import org.junit.jupiter.api.Assertions;
+import ch1.Container;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UnitTests {
-    private Con
+    private Container a, b;
+
+    @BeforeEach
+    void beforeEach() {
+        a = new Container();
+        b = new Container();
+    }
 
     @Test
-    void name() {
-//        assertThrows()
+    void testNewContainerIsEmpty() {
+        String message = "new container is not empty";
+
+        assertTrue(a.getAmount() == 0, message);
+        assertEquals(a.getAmount(), 0, 0, message);
+        assertThat(message, a.getAmount(), closeTo(0, 0));
     }
 }
