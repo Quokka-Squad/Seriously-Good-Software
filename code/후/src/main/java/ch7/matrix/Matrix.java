f97c9436c651888f4552b024131bfc6acae06929
who-hoo <who.ho3ov@gmail.com>
2022-02-19 02:37:55 +0900

    :bulb: 문서화 주석 작성

--- code/후/src/main/java/ch7/matrix/Matrix.java
+++ code/후/src/main/java/ch7/matrix/Matrix.java
@@ -2,6 +2,12 @@ package ch7.matrix;
 
 public class Matrix {
 
+    /**
+     * 정사각 행렬을 전치한다.
+     *
+     * @param matrix 행렬
+     * @throws IllegalArgumentException 주어진 행렬이 정사각이 아님
+     */
     public static void transpose(double[][] matrix) {
         if (!isSquare(matrix)) {
             throw new IllegalArgumentException("주어진 행렬이 정사각형이 아닙니다.");
@@ -15,6 +21,12 @@ public class Matrix {
         }
     }
 
+    /**
+     * 행렬이 정사각인지 확인한다.
+     *
+     * @param matrix 행렬
+     * @return 행렬이 정사각이면 {@code true}
+     */
     public static boolean isSquare(double[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             if (matrix[i].length != matrix.length) {
