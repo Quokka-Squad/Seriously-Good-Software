7cbaa9bf0c0f3130b68920120a495439c69e84ca
who-hoo <who.ho3ov@gmail.com>
2022-02-19 02:27:21 +0900

    :recycle: 정사각 행렬인지 검증하는 로직 메서드 분리

--- code/후/src/main/java/ch7/matrix/Matrix.java
+++ code/후/src/main/java/ch7/matrix/Matrix.java
@@ -3,10 +3,8 @@ package ch7.matrix;
 public class Matrix {
 
     public static void f(double[][] a) {
-        for (int i = 0; i < a.length; i++) {
-            if (a[i].length != a.length) {
-                throw new IllegalArgumentException();
-            }
+        if (!isSquare(a)) {
+            throw new IllegalArgumentException("주어진 행렬이 정사각형이 아닙니다.");
         }
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < i; j++) {
@@ -16,5 +14,14 @@ public class Matrix {
                 j++;
             }
         }
+    }
+
+    public static boolean isSquare(double[][] a) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i].length != a.length) {
+                return false;
+            }
+        }
+        return true;
     }
 }
