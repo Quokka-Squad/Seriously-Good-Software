e61ed5ce20a2ed180393292c69b5acadc69eeb5d
who-hoo <who.ho3ov@gmail.com>
2022-02-19 02:01:07 +0900

    :bulb: 자바독을 이용한 클래스 헤더 문서화

--- code/후/src/main/java/ch7/readable/Container.java
+++ code/후/src/main/java/ch7/readable/Container.java
@@ -3,6 +3,18 @@ package ch7.readable;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * A <code>Container</code>는 사실상 용량 제한이 없는 수조를 나타낸다.
+ * <p>
+ * 물은 넣거나 뺄 수 있다. 두 수조는 영구적인 파이프로 연결될 수 있다. 두 수조를 직/간접적으로 연결하면 두 수조는 상호작용을 주고받으며 물은 둘 사이에
+ * <b>균등하게</b> 배분된다.
+ * <p>
+ * 이 수조에 연결된 모든 수조의 집합을 이 수조의
+ * <i>group</i> 이라고 한다.
+ *
+ * @author Marco Faella
+ * @version 1.0
+ */
 public class Container {
 
     private Set<Container> group;
