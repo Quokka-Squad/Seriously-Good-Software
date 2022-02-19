9d6b18c504273ef75b087dda10e0a337bff660b4
who-hoo <who.ho3ov@gmail.com>
2022-02-19 15:40:03 +0900

    :bulb: 자바독 메서드 주석 추가

--- code/후/src/main/java/ch7/practice/Practice.java
+++ code/후/src/main/java/ch7/practice/Practice.java
@@ -2,6 +2,12 @@ package ch7.practice;
 
 public class Practice {
 
+    /**
+     * 문자열 s에서 문자 c가 나타나는 수를 세어 반환한다.
+     * @param s 문자열
+     * @param c 몇 번 나타는지 세어볼 문자
+     * @return 문자열 s에서 문자 c가 나타나는 수
+     */
     public static int countOccurrences(String s, char c) {
         return (int) s.chars()
             .filter(character -> character == c)
