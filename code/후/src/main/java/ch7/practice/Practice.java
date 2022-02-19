749cb7130724ea71e38693739059f9c3d465ecac
who-hoo <who.ho3ov@gmail.com>
2022-02-19 16:31:26 +0900

    :recycle: 좀 더 의미를 명확하게 전달하는 적절한 변수명으로 변경

--- code/후/src/main/java/ch7/practice/Practice.java
+++ code/후/src/main/java/ch7/practice/Practice.java
@@ -20,30 +20,33 @@ public class Practice {
     /**
      * BFS를 코드로 구현해 사용
      *
-     * @param a 그래프 탐색을 수행할 구조. 인접 행렬 등
-     * @param vertices 사용할 정점
-     * @param source 소스
+     * @param adjacentMatrix 그래프 탐색을 수행할 인접 행렬
+     * @param vertexCount    정점의 수
+     * @param sourceVertex   시작 정점
      */
-    public static void bfsImplement(byte [][] a,int vertices,int source){  // 인접 행렬과 정점의 수를 전달
-        byte []b=new byte[vertices];    // 각 정점의 상태를 저장할 플래그 컨테이너
-        Arrays.fill(b,(byte)-1);   // 상태 초기화
+    public static void bfsImplement(byte[][] adjacentMatrix, int vertexCount,
+        int sourceVertex) {  // 인접 행렬과 정점의 수를 전달
+        byte[] b = new byte[vertexCount];    // 각 정점의 상태를 저장할 플래그 컨테이너
+        Arrays.fill(b, (byte) -1);   // 상태 초기화
        /*       code   status
                 -1  =  ready
                  0  =  waiting
                  1  =  processed       */
 
         Stack<Integer> st = new Stack<>();     // 운영 스택
-        st.push(source);                                     // 소스 할당
-        while(!st.isEmpty()){
-            b[st.peek()]=(byte)0;                      // 대기 상태 할당
+        st.push(sourceVertex);                                     // 소스 할당
+        while (!st.isEmpty()) {
+            b[st.peek()] = (byte) 0;                      // 대기 상태 할당
             System.out.println(st.peek());
-            int pop=st.peek();
-            b[pop]=(byte)1;               // 처리된 상태로 썰정
+            int pop = st.peek();
+            b[pop] = (byte) 1;               // 처리된 상태로 썰정
             st.pop();                  // 큐의 헤드 제거
-            for(int i=0;i<vertices;i++){
-                if(a[pop][i]!=0 && b[i]!=(byte)0 && b[i]!=(byte)1 ){
+            for (int i = 0; i < vertexCount; i++) {
+                if (adjacentMatrix[pop][i] != 0 && b[i] != (byte) 0 && b[i] != (byte) 1) {
                     st.push(i);
-                    b[i]=(byte)0;                        // 대기 상태 할당
-                }}}
+                    b[i] = (byte) 0;                        // 대기 상태 할당
+                }
+            }
+        }
     }
 }
