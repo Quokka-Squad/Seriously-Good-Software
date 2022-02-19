a4ce518b4be95ca60461a3fcf405a0d0e5c01444
who-hoo <who.ho3ov@gmail.com>
2022-02-19 15:39:37 +0900

    :recycle: 가독성 개선

--- code/후/src/main/java/ch7/practice/Practice.java
+++ code/후/src/main/java/ch7/practice/Practice.java
@@ -2,19 +2,9 @@ package ch7.practice;
 
 public class Practice {
 
-    public static int f(String s, char c) {
-        int i = 0, n = 0;
-        boolean flag = true;
-        while (flag) {
-            if (s.charAt(i) == c) {
-                n++;
-            }
-            if (i == s.length() - 1) {
-                flag = false;
-            } else {
-                i++;
-            }
-        }
-        return n;
+    public static int countOccurrences(String s, char c) {
+        return (int) s.chars()
+            .filter(character -> character == c)
+            .count();
     }
 }
